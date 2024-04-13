160b97dd4ced24088eceba40da810acea604d1de
billyweiye <wyeah1980@gmail.com>
2024-04-14 02:03:28 +0800

    new_version_api

--- 51jobcrawler.py
+++ 51jobcrawler.py
@@ -1,109 +1,119 @@
 #impport modules
 import requests
 import re
-from bs4 import BeautifulSoup
-import os
-import pandas as pd
 import time
 from urllib.request import quote
+from acwCookie import getAcwScV2
 
 
+class JobSearch:
+    def __init__(self, url, cookies,headers):
+        self.url = url
+        self.headers = headers
+        self.cookies=cookies
+        self.max_retry=10
 
-#get the html text from the webpage
-def getHTMLText(url):
-    try:
-        r=requests.get(url,timeout=30)
-        r.raise_for_status()
-        r.encoding=r.apparent_encoding
-        html=r.text
-        print('html is ready')
-        return html
-    except:
-        print('get html text failed')
+    #get the code of the city 
+    def citycoder(city):
+        url='https://js.51jobcdn.com/in/js/2016/layer/area_array_c.js'
+        r= requests.get(url,timeout=30)
+        fl=r.text
+        geocode = re.findall('"([0-9]+)":"{}"'.format(city),fl)[0]
+        return geocode
 
-#get the code of the city 
-def citycoder(city):
-    url='https://js.51jobcdn.com/in/js/2016/layer/area_array_c.js'
-    r= requests.get(url,timeout=30)
-    fl=r.text
-    geocode = re.findall('"([0-9]+)":"{}"'.format(city),fl)[0]
-    return geocode
-
-#soup the html file
-def soupmaker(html):
-    soup = BeautifulSoup(html,'html.parser')
-    return soup 
-    print('soup is ready')
-
-#extract data we want from the soup
-def listfiller(tags): #get the information we need
-    jobtitle=list()
-    joburl=list()
-    company=list()
-    location=list()
-    salary=list()
-    for tag in tags:
-        try:
-            if 't1' in tag.get('class',None):
-                jobtitle.append(tag.find('a').string.strip())
-                joburl.append(tag.find('a').get('href',None))
-            if 't2' in tag.get('class',None):
-                company.append(tag.string)
-            if 't3' in tag.get('class',None):
-                location.append(tag.string)
-            if 't4' in tag.get('class',None):
-                salary.append(tag.string)
+    def search_jobs(self, params):
+        self.max_retry=self.max_retry-1
+        if self.max_retry>=0:
+            response = requests.get(self.url,cookies=self.cookies, headers=self.headers, params=params)
+            pattern = r"var arg1='([A-F0-9]+)';"
+            # Search for the pattern in the JavaScript code
+            arg1 = re.search(pattern, response.text)
+            if arg1:
+                print(f"retry:{10-self.max_retry} set_cookies")
+                arg1=arg1.group(1)
+                self.cookies['acw_sc__v2']=getAcwScV2(arg1)
+                response = self.search_jobs(params)
             else:
-                continue
-        except:
-            continue
-    company=company[1:]
-    location=location[1:]
-    salary=salary[1:]
-    print('list has been filled')
-    return jobtitle,joburl,company,location,salary
+                self.max_retry=10
+        else:
+            raise Exception("Max Retry Exceeded!")
+        
 
+        return response
 
-
-
-jobtitle=[]
-joburl=[]
-company=[]
-location=[]
-salary=[]
-
-kw = quote(input('职位名称').strip())
-dump=int(int(input('爬取多少页').strip())+1)
-geocode=citycoder(str(input('城市').strip()))
-for page in range(1,dump):
-    url='https://search.51job.com/list/{},000000,0000,00,9,99,{},2,{}.html'.format(geocode,kw,page)
-    print(url)
-    html = getHTMLText(url)
-    soup = soupmaker(html)
+    def get_jobs_json(self,params):
+        return self.search_jobs(params).json()
     
-    tags=soup(class_=['t1','t2','t3','t4'])
-    L=listfiller(tags)
-
-    jobtitle += L[0]
-    joburl+=L[1]
-    company+=L[2]
-    location+=L[3]
-    salary+=L[4]
-
-    time.sleep(2) # Slow things down so as to not hammer Wikipedia's servers
+    
+url = "https://we.51job.com/api/job/search-pc"
 
 
-
-
-raw = {
-    'jobtitle' : jobtitle,
-    'company': company,
-    'location': location,
-    'salary': salary,
-    'link': joburl
+initial_cookies={
+    'privacy':str(int(time.time())) ,
+    'guid':'21ae369c1fecc95608a454bacdd16b41',
+    'acw_tc':'ac11000117130121464015687e00d6b80852550fe03e0fd08cf69bf654d5a3',
+    'JSESSIONID':'2F0AFC4C5819D899810516F3424C7A87',
+    'NSC_ohjoy-bmjzvo-200-159':'ffffffffc3a0d42e45525d5f4f58455e445a4a423660',
 }
 
-df = pd.DataFrame(raw)
-print('dataframe has been created')
 
-df.head(10)
+headers = {
+  'Accept': 'application/json,text/plain,*/*',
+  'Accept-Encoding': 'gzip,deflate,br,zstd',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Connection': 'keep-alive',
+  'DNT': '1',
+  'From-Domain': '51job_web',
+  'Host': 'we.51job.com',
+  'Referer': 'https://we.51job.com/pc/search?jobArea=000000&keyword=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&searchType=2',
+  'Sec-Fetch-Dest': 'empty',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+  'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_15_7)AppleWebKit/537.36(KHTML,likeGecko)Chrome/123.0.0.0Safari/537.36',
+  'account-id': '',
+  'partner': '',
+  'property': '%7B%22partner%22%3A%22%22%2C%22webId%22%3A2%2C%22fromdomain%22%3A%2251job_web%22%2C%22frompageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2F%22%2C%22pageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2Fpc%2Fsearch%3FjobArea%3D000000%26keyword%3D%25E6%2595%25B0%25E6%258D%25AE%25E5%2588%2586%25E6%259E%2590%26searchType%3D2%22%2C%22identityType%22%3A%22%22%2C%22userType%22%3A%22%22%2C%22isLogin%22%3A%22%E5%90%A6%22%2C%22accountid%22%3A%22%22%2C%22keywordType%22%3A%22%22%7D',
+  'sec-ch-ua': '"GoogleChrome";v="123","Not:A-Brand";v="8","Chromium";v="123"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sign': 'f858c5f418f30e4dd65f3c7f86d03a8e87e87302695161c82a9b37f04d790287',
+  'user-token': '',
+  'uuid': '21ae369c1fecc95608a454bacdd16b4'
+}
+
+kw='Data Analyst'
+cities=['010000']
+page_num=1
+user_params = {
+    'api_key': '51job',
+    'timestamp': f'1712988972{int(time.time())}',
+    'keyword': f'{kw}',
+    'searchType': '2',
+    'function': '',
+    'industry': '',
+    'jobArea': f"{'%'.join(cities)}",
+    'jobArea2': '',
+    'landmark': '',
+    'metro': '',
+    'salary': '',
+    'workYear': '',
+    'degree': '',
+    'companyType': '',
+    'companySize': '',
+    'jobType': '',
+    'issueDate': '',
+    'sortType': '1',
+    'pageNum': f'{page_num}',
+    'pageSize': '20',
+    'source': '1',
+    'accountId': '',
+    'pageCode': 'sou%7Csou%7Csoulb',
+    'userLonLat': ''
+}
+
+
+
+
+jobs = JobSearch(url, initial_cookies,headers)
+res = jobs.search_jobs(user_params)
+res_json=jobs.get_jobs_json(params=user_params)
