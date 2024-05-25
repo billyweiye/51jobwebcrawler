c569f2574ca6cd8662b3d5f925f2d4486b7c3df6
billyweiye <wyeah1980@gmail.com>
2024-05-25 23:49:06 +0800

    modified

--- 51jobcrawler.py
+++ 51jobcrawler.py
@@ -53,132 +53,138 @@ class JobSearch:
         return self.search_jobs(params).json()
     
 
+if __name__=="__main__":
+
+
+
+    # 读取配置文件
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    # 获取配置值
+    app_id = config['tokens']['app_id']
+    app_secret = config['tokens']['app_secret']
+
+    fs=Feishu(app_id=app_id,app_secret=app_secret)
+
+    db_id = config['db_info']['db_id']
+    table_id = config['db_info']['table_id']
+
     
-    
-url = "https://we.51job.com/api/job/search-pc"
+        
+    url = "https://we.51job.com/api/job/search-pc"
 
 
-initial_cookies={
-    'privacy':str(int(time.time())) ,
-    'guid':'21ae369c1fecc95608a454bacdd16b41',
-    'acw_tc':'ac11000117130121464015687e00d6b80852550fe03e0fd08cf69bf654d5a3',
-    'JSESSIONID':'2F0AFC4C5819D899810516F3424C7A87',
-    'NSC_ohjoy-bmjzvo-200-159':'ffffffffc3a0d42e45525d5f4f58455e445a4a423660',
-}
-
-
-headers = {
-  'Accept': 'application/json,text/plain,*/*',
-  'Accept-Encoding': 'gzip,deflate,br,zstd',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'Connection': 'keep-alive',
-  'DNT': '1',
-  'From-Domain': '51job_web',
-  'Host': 'we.51job.com',
-  'Referer': 'https://we.51job.com/pc/search?jobArea=000000&keyword=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&searchType=2',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_15_7)AppleWebKit/537.36(KHTML,likeGecko)Chrome/123.0.0.0Safari/537.36',
-  'account-id': '',
-  'partner': '',
-  'property': '%7B%22partner%22%3A%22%22%2C%22webId%22%3A2%2C%22fromdomain%22%3A%2251job_web%22%2C%22frompageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2F%22%2C%22pageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2Fpc%2Fsearch%3FjobArea%3D000000%26keyword%3D%25E6%2595%25B0%25E6%258D%25AE%25E5%2588%2586%25E6%259E%2590%26searchType%3D2%22%2C%22identityType%22%3A%22%22%2C%22userType%22%3A%22%22%2C%22isLogin%22%3A%22%E5%90%A6%22%2C%22accountid%22%3A%22%22%2C%22keywordType%22%3A%22%22%7D',
-  'sec-ch-ua': '"GoogleChrome";v="123","Not:A-Brand";v="8","Chromium";v="123"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"macOS"',
-  'sign': 'f858c5f418f30e4dd65f3c7f86d03a8e87e87302695161c82a9b37f04d790287',
-  'user-token': '',
-  'uuid': '21ae369c1fecc95608a454bacdd16b4'
-}
-
-
-
-result=[]
-page=1
-max_page=50
-
-# initialize search engine
-jobs = JobSearch(url, initial_cookies,headers)
-
-while True:
-    print("Page Num: ",page)
-    kw='data analyst'
-    cities=['020000']
-    user_params = {
-        'api_key': '51job',
-        'timestamp': f'{int(time.time())}',
-        'keyword': kw,
-        'searchType': '2',
-        'function': '',
-        'industry': '',
-        'jobArea': f"{'%'.join(cities)}",
-        'jobArea2': '',
-        'landmark': '',
-        'metro': '',
-        'salary': '',
-        'workYear': '',
-        'degree': '',
-        'companyType': '',
-        'companySize': '',
-        'jobType': '',
-        'issueDate': '',
-        'sortType': '1',
-        'pageNum': page,
-        'pageSize': '20',
-        'source': '1',
-        'accountId': '',
-        'pageCode': 'sou%7Csou%7Csoulb',
-        'userLonLat': ''
+    initial_cookies={
+        'privacy':str(int(time.time())) ,
+        'guid':'21ae369c1fecc95608a454bacdd16b41',
+        'acw_tc':'ac11000117130121464015687e00d6b80852550fe03e0fd08cf69bf654d5a3',
+        'JSESSIONID':'2F0AFC4C5819D899810516F3424C7A87',
+        'NSC_ohjoy-bmjzvo-200-159':'ffffffffc3a0d42e45525d5f4f58455e445a4a423660',
     }
 
-    res_json=jobs.get_jobs_json(params=user_params)
 
-    if res_json is not str:
-        if res_json['resultbody']['job']['items']:
+    headers = {
+    'Accept': 'application/json,text/plain,*/*',
+    'Accept-Encoding': 'gzip,deflate,br,zstd',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'From-Domain': '51job_web',
+    'Host': 'we.51job.com',
+    'Referer': 'https://we.51job.com/pc/search?jobArea=000000&keyword=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&searchType=2',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_15_7)AppleWebKit/537.36(KHTML,likeGecko)Chrome/123.0.0.0Safari/537.36',
+    'account-id': '',
+    'partner': '',
+    'property': '%7B%22partner%22%3A%22%22%2C%22webId%22%3A2%2C%22fromdomain%22%3A%2251job_web%22%2C%22frompageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2F%22%2C%22pageUrl%22%3A%22https%3A%2F%2Fwe.51job.com%2Fpc%2Fsearch%3FjobArea%3D000000%26keyword%3D%25E6%2595%25B0%25E6%258D%25AE%25E5%2588%2586%25E6%259E%2590%26searchType%3D2%22%2C%22identityType%22%3A%22%22%2C%22userType%22%3A%22%22%2C%22isLogin%22%3A%22%E5%90%A6%22%2C%22accountid%22%3A%22%22%2C%22keywordType%22%3A%22%22%7D',
+    'sec-ch-ua': '"GoogleChrome";v="123","Not:A-Brand";v="8","Chromium";v="123"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sign': 'f858c5f418f30e4dd65f3c7f86d03a8e87e87302695161c82a9b37f04d790287',
+    'user-token': '',
+    'uuid': '21ae369c1fecc95608a454bacdd16b4'
+    }
 
 
-            result.append(res_json)
-    else:
-        print(res_json)
+    kws=['data analyst','business analyst','crm analyst','sap analyst','BI','data scientist','data engineer']
 
-    page+=1
+    for kw in kws:
 
-    if page>max_page:
-        break
+        result=[]
+        page=1
+        max_page=3
 
-    time.sleep(random.randint(1,8))
+        # initialize search engine
+        jobs = JobSearch(url, initial_cookies,headers)
 
-df=pd.DataFrame()
-for page in result:
-    df=df.append(pd.DataFrame(data=page['resultbody']['job']['items']))
+        while True:
+            print("Page Num: ",page)
+            cities=['020000']
+            user_params = {
+                'api_key': '51job',
+                'timestamp': f'{int(time.time())}',
+                'keyword': kw,
+                'searchType': '2',
+                'function': '',
+                'industry': '',
+                'jobArea': f"{'%'.join(cities)}",
+                'jobArea2': '',
+                'landmark': '',
+                'metro': '',
+                'salary': '',
+                'workYear': '',
+                'degree': '',
+                'companyType': '',
+                'companySize': '',
+                'jobType': '',
+                'issueDate': '',
+                'sortType': '1',
+                'pageNum': page,
+                'pageSize': '20',
+                'source': '1',
+                'accountId': '',
+                'pageCode': 'sou%7Csou%7Csoulb',
+                'userLonLat': ''
+            }
 
-df.to_csv(f"result_{kw}_{int(time.time())}.csv",index=False)
+            res_json=jobs.get_jobs_json(params=user_params)
+
+            if res_json is not str:
+                if res_json['resultbody']['job']['items']:
 
 
+                    result.append(res_json)
+            else:
+                print(res_json)
 
+            page+=1
 
+            if page>max_page:
+                break
 
-# 读取配置文件
-config = configparser.ConfigParser()
-config.read('config.ini')
+            time.sleep(random.randint(1,8))
 
-# 获取配置值
-app_id = config['tokens']['app_id']
-app_secret = config['tokens']['app_secret']
+        df=pd.DataFrame()
+        for page in result:
+            df=df.append(pd.DataFrame(data=page['resultbody']['job']['items']))
+        df['search_kw']=kw
 
-fs=Feishu(app_id=app_id,app_secret=app_secret)
+        
 
-db_id = config['db_info']['db_id']
-table_id = config['db_info']['table_id']
+        #写入飞书多维表
+        fields= [
+            {
+            "fields": {key: str(value) for key, value in row.items()}
+            } 
+            for row in df.to_dict("records") 
+        ]
 
-fields= [
-    {
-      "fields": {key: str(value) for key, value in row.items()}
-      } 
-      for row in df.to_dict("records") 
-  ]
+        for i in range(1,len(fields)//400+2):
+            res=fs.add_records(db_id,table_id,fields[400*(i-1):400*i])
 
-for i in range(1,len(fields)//400+2):
-    res=fs.add_records(db_id,table_id,fields[400*(i-1):400*i])
-
-    time.sleep(0.3)
+            time.sleep(0.3)
+        
+        time.sleep(random.randint(1,30))
--- feishu_doc.py
+++ feishu_doc.py
@@ -2,26 +2,51 @@
 import requests
 import json
 
+
 class Feishu:
-    def __init__(self,token) -> None:
-        self.token=f'Bearer {token}'
+    def __init__(self,app_id,app_secret) -> None:
+       # self.token=f'Bearer {token}'
+       self.app_id=app_id
+       self.app_secret=app_secret
+       self.tenant_access_token=self.get_tenant_access_token()
 
-    def create_db(self,folder_id,title):
 
-        url = "https://open.feishu.cn/open-apis/bitable/v1/apps"
+
+    def get_tenant_access_token(self):
+
+        url = "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
         payload = json.dumps({
-            "folder_token": folder_id,
-            "name": title
+            "app_id": self.app_id,
+            "app_secret": self.app_secret
         })
 
 
         headers = {
-        'Content-Type': 'application/json',
-        'Authorization': self.token
+        'Content-Type': 'application/json'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-        print(response.text)
+        return response.json()["tenant_access_token"]
+    
+
+
+
+    # def create_db(self,folder_id,title):
+
+    #     url = "https://open.feishu.cn/open-apis/bitable/v1/apps"
+    #     payload = json.dumps({
+    #         "folder_token": folder_id,
+    #         "name": title
+    #     })
+
+
+    #     headers = {
+    #     'Content-Type': 'application/json',
+    #     'Authorization': f'Bearer {self.user_access_token}'
+    #     }
+
+    #     response = requests.request("POST", url, headers=headers, data=payload)
+    #     print(response.text)
 
 
     def create_table(self,db_id,table_name,default_view_name,fields:list):
@@ -46,7 +71,7 @@ class Feishu:
 
         headers = {
         'Content-Type': 'application/json',
-        'Authorization': self.token
+        'Authorization': f'Bearer {self.tenant_access_token}'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
@@ -65,7 +90,7 @@ class Feishu:
 
         headers = {
         'Content-Type': 'application/json',
-        'Authorization': self.token
+        'Authorization': f'Bearer {self.tenant_access_token}'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
@@ -75,6 +100,7 @@ class Feishu:
 
     def add_records(self,db_id,table_id,fields:list):
 
+
         url = f"https://open.feishu.cn/open-apis/bitable/v1/apps/{db_id}/tables/{table_id}/records/batch_create"
         payload = json.dumps({
             "records": fields
@@ -83,7 +109,7 @@ class Feishu:
 
         headers = {
         'Content-Type': 'application/json',
-        'Authorization': self.token
+        'Authorization': f'Bearer {self.tenant_access_token}'
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
